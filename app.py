fc71da3c5cdf8e0cece03d708ab296a30a1305cd
aranamor <wapnajitd@gmail.com>
2024-09-14 22:10:48 +0530

    Redis implemented

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, jsonify
+import redis
+import json
+from flask import Flask, jsonify, request
 from scraper import start_scraper_thread
 
+cache = redis.StrictRedis(host='localhost', port=6379, db=0)
 app = Flask(__name__)
-
-# Start scrapper
 start_scraper_thread()
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    text = request.args.get('text', default='', type=str)
+    cached_result = cache.get(text)
+    if cached_result:
+        return jsonify({"source": "cache", "results": json.loads(cached_result)})
+
+    result = f"Search results for {text}"
+
+    cache.set(text, json.dumps(result), ex=3600)  # Set expiry of 1 hour
+
+    return jsonify({"source": "server", "results": result})
 
 
 @app.route('/health', methods=['GET'])
